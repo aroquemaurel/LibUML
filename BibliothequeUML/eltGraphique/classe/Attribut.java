7c81f1a6a427e1371d97cf4cc362f5a5a0f7bd83
Mathieu Soum <mathieu.soum@gmail.com>
2011-12-10 18:23:50 +0100

    Variable() et Attribut() implémentées

--- BibliothequeUML/eltGraphique/classe/Attribut.java
+++ BibliothequeUML/eltGraphique/classe/Attribut.java
@@ -6,6 +6,14 @@ package eltGraphique.classe;
  */
 class Attribut extends Variable {
     private Visibilite visibilite;
+    private boolean deClasse;
+	public Attribut(Visibilite p_visibilite,
+					boolean p_deClasse, boolean p_constante,
+					String p_type, String p_nom) {
+		super(p_constante, p_type, p_nom);
+		this.deClasse = p_deClasse;
+		this.visibilite = p_visibilite;
+	}
 
     public Visibilite getVisibilite() {
         return (this.visibilite);
@@ -14,4 +22,12 @@ class Attribut extends Variable {
     public void setVisibilite(Visibilite p_visibilite) {
         this.visibilite = p_visibilite;
     }
+
+	public boolean isDeClasse() {
+		return deClasse;
+	}
+
+	public void setDeClasse(boolean deClasse) {
+		this.deClasse = deClasse;
+	}
 }
--- BibliothequeUML/eltGraphique/classe/Variable.java
+++ BibliothequeUML/eltGraphique/classe/Variable.java
@@ -5,8 +5,15 @@ package eltGraphique.classe;
  * @author spongebob
  */
 abstract class Variable {
-    private String type;
     private boolean constante;
+    private String type;
+    private String nom;	
+    
+    public Variable(boolean p_constante, String p_type, String p_nom) {
+	    this.constante = p_constante;
+	    this.type = p_type;
+	    this.nom = p_nom;
+    }
 
     public boolean isConstante() {
         return (this.constante);
@@ -23,4 +30,12 @@ abstract class Variable {
     public void setType(String p_type) {
         this.type = p_type;
     }
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 }
