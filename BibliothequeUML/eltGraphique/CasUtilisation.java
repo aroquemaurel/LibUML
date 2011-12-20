53127dd13b2434bda01a5a4507717224dd0fa956
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2011-12-20 19:03:09 +0100

    Classe CasUtilisation -- getters/setters/constructeur

--- BibliothequeUML/eltGraphique/CasUtilisation.java
+++ BibliothequeUML/eltGraphique/CasUtilisation.java
@@ -1,9 +1,35 @@
 package eltGraphique;
 
+import com.mxgraph.model.mxCell;
+import com.mxgraph.view.mxGraph;
+
 /**
  *
  * @author satenske
  */
 public class CasUtilisation extends eltGraphique.ElementModelisation {
-    private String nom;
+    private mxGraph graph;
+    private mxCell cellule;
+
+    public CasUtilisation(mxGraph p_graph, String p_nom){
+        this.graph = p_graph;
+        super.setNom(p_nom);
+    }
+    
+    public void creer(){
+        Object parent = this.graph.getDefaultParent();
+        this.cellule = (mxCell) this.graph.insertVertex(
+            parent, null, super.getNom(), 30, 30, 150, 75, "USECASE");
+        this.cellule.setVisible(false);
+    }
+  
+    public void afficher(){
+        this.cellule.setVisible(true);     
+    }
+    
+    @Override
+    public void setNom(String p_nom){
+        super.setNom(p_nom);
+        this.cellule.setValue(p_nom);
+    }
 }
