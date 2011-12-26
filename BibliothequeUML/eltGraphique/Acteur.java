1612dc355209b9834495b4105bb48889edb3362c
Mathieu Soum <mathieu.soum@gmail.com>
2011-12-26 14:25:54 +0100

    Ajout des commentaires javadoc à Acteur.java

--- BibliothequeUML/eltGraphique/Acteur.java
+++ BibliothequeUML/eltGraphique/Acteur.java
@@ -14,27 +14,71 @@ import com.mxgraph.view.mxGraph;
  * @author satenske
  */
 abstract public class Acteur extends ElementModelisation {
+	/**
+	 * Le graphe auquel est ajouté l'acteur
+	 */
     private mxGraph graph;
+	/**
+	 * La célulle que l'acteur représente sur le graphe
+	 */
     private mxCell cellule;
 
+	/**
+	 * Mettre abstraite
+	 * 
+	 * @see ActeurActif
+	 * @see ActeurPassif
+	 */
     abstract public void creer();
   
+	/**
+	 * Mettre abstraite
+	 * 
+	 * @see ActeurActif
+	 * @see ActeurPassif
+	 */
     abstract public void afficher();
-        
+    
+	/**
+	 * Constructeur le la classe Acteur
+	 * 
+	 * @param p_graph le graphe sur lequel il faut ajouter l'acteur
+	 * @param p_texte le texte qui sera afficher sur l'acteur
+	 * 
+	 */
     public Acteur(mxGraph p_graph, String p_texte){
         this.graph = p_graph;
         super.setTexte(p_texte);
     }
+	
+	/**
+	 * 
+	 * @return La cellule représentée par l'acteur
+	 */
     public mxCell getCellule(){
         return this.cellule;
     }
+	
+	/**
+	 * 
+	 * @param p_cellule La cellule représentée par l'acteur
+	 */
     public void setCellule(mxCell p_cellule){
         this.cellule = p_cellule;
     }
+	
+	/**
+	 * 
+	 * @return Le graphe dans lequel est l'acteur
+	 */
     public mxGraph getGraph(){
         return this.graph;
     }
 
+	/**
+	 * 
+	 * @param p_texte Le texte lié à l'acteur
+	 */
     @Override
     public void setTexte(String p_texte){
         super.setTexte(p_texte);
