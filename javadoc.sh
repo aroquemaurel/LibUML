ee9449f32b0f4496901fca5c30918ba4f4129805
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2012-01-03 21:09:36 +0100

    Gestion encodage utf8 javadoc

--- javadoc.sh
+++ javadoc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
 cd BibliothequeUML
-javadoc -d ../documentation eltGraphique/*.java eltGraphique/classe/*.java eltGraphique/ligne/*.java diagramme/*.java -charset utf-8 -windowtitle "Bibliotheque UML" > ../javadoc.log 2>&1 
+javadoc -d ../documentation eltGraphique/*.java eltGraphique/classe/*.java eltGraphique/ligne/*.java diagramme/*.java -charset utf8 -windowtitle "Bibliotheque UML" > ../javadoc.log 2>&1 
 cd ..
