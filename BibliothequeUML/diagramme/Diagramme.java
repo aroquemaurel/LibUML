540189873989c88535b6a61177846144fbd2371c
Marie-Ly Tang <marie-ly.tang@hotmail.fr>
2012-01-04 15:35:18 +0100

    Implementation methodes lienAutorise eltAutorise Diagramme DiagrammeClasse

--- BibliothequeUML/diagramme/Diagramme.java
+++ BibliothequeUML/diagramme/Diagramme.java
@@ -1,9 +1,21 @@
 package diagramme;
 
+import eltGraphique.EltGraphique;
+import java.util.List;
+
 /**
  *
  * @author satenske
  */
 public class Diagramme {
-
+     /**
+     * liste des éléments graphiques
+     */
+    private List<EltGraphique> eltGraphique;
+    public boolean lienAutorise (EltGraphique origine , EltGraphique extremite){
+        return true;
+    }
+    public boolean eltAutorise (EltGraphique p_element){
+        return true;
+    }
 }
--- BibliothequeUML/diagramme/DiagrammeClasse.java
+++ BibliothequeUML/diagramme/DiagrammeClasse.java
@@ -1,9 +1,24 @@
-package diagramme;
+ package diagramme;
+
+import eltGraphique.EltGraphique;
+import eltGraphique.classe.Classe;
+import eltGraphique.ligne.Lien;
 
 /**
  *
  * @author satenske
  */
 public class DiagrammeClasse extends Diagramme {
-
+    @Override
+    public boolean lienAutorise (EltGraphique origine , EltGraphique extremite){
+        boolean valeurRetour = false;
+        if(origine instanceof Classe && extremite instanceof Classe){
+            valeurRetour = true;
+        }
+        if(origine instanceof Lien && extremite instanceof Classe){
+            valeurRetour = true;
+        }
+        
+        return(valeurRetour);
+    }
 }
