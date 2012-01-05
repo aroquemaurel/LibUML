61d2347b8b73b6303352c75283b4500e84d94685
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-05 10:15:01 +0100

    Modification des commentaires javadoc et ArrayList --> List pour Classe.java

--- BibliothequeUML/eltGraphique/Interface.java
+++ BibliothequeUML/eltGraphique/Interface.java
@@ -7,22 +7,22 @@ import com.mxgraph.view.mxGraph;
  * 
  */
 public class Interface extends ElementModelisation {
-	/**
-	 * Constructeur de la classe traitement
-	 * @param p_graph Le graphe auquel sera ajouter le traitement
-	 * @param p_texte Le texte qui sera associé au traitement
-	 */
+    /**
+     * Constructeur de la classe traitement
+     * @param p_graph Le graphe auquel sera ajouter le traitement
+     * @param p_texte Le texte qui sera associé au traitement
+     */
     public Interface(mxGraph p_graph, String p_texte){
         super(p_graph, p_texte);
-		this.creer();
+	this.creer();
     }
     
 
-	/**
-	 * Crée la représentation graphique d'un traitement
-	 */
+    /**
+     * Crée la représentation graphique d'un traitement
+     */
     @Override
-    public void creer() {
+    public final void creer() {
         //TODO
     }
 }
