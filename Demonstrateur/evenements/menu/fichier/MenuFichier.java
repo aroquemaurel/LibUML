d1aba96c423d8d948e9efc9f88895fe16d20eed6
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2012-01-05 12:05:55 +0100

    Modification commentaires MenuFichier

--- Demonstrateur/evenements/menu/fichier/MenuFichier.java
+++ Demonstrateur/evenements/menu/fichier/MenuFichier.java
@@ -9,13 +9,17 @@ import javax.swing.JOptionPane;
  * superclasse des évenements sur les éléments du menu.
  */
 public class MenuFichier extends JMenuItem implements ActionListener{
+	/* Paramètres du menu */
 	private void parametrerMenu(){
 		this.addActionListener(this);
 	}
+
+	/* Construit le menu avec comme titre p_texte */
 	public MenuFichier(String p_texte){
 		super.setText(p_texte);
 		this.parametrerMenu();
 	}
+
 	 /* Si les classes filles ne redéfinissent pas l'implémentation en cas d'action sur le menu,
 	  * on affiche un message.
 	  */
