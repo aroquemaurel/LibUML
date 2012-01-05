81cc951390985de60054d0dafff4264b7de62358
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2012-01-05 10:11:27 +0100

    Commentaires BarreOutilsDessin

--- Demonstrateur/ihm/menu/BarreOutilsDessin.java
+++ Demonstrateur/ihm/menu/BarreOutilsDessin.java
@@ -15,37 +15,48 @@ import java.awt.Dimension;
 import javax.swing.JToolBar;
 
 /**
- *
- * @author satenske
+ * Barre d'outils permettant d'afficher les différents outils de dessins
  */
 public class BarreOutilsDessin extends JToolBar {
-	private final FenetreDemo fenetre;
+	private final FenetreDemo fenetre; // La fenêtre dans laquelle s'affiche la toolbar
+	private final Dimension taille;
 
-	public BarreOutilsDessin(Dimension p_taille, FenetreDemo p_fenetre) {
-		this.setPreferredSize(p_taille);
+	/* Paramètres de la barre d'outils */
+    private void parametrerBarreOutils(){
+		this.setPreferredSize(this.taille);
         this.setFloatable(false);
+    }
+
+	/* Construit la barre d'outils */
+	public BarreOutilsDessin(Dimension p_taille, FenetreDemo p_fenetre) {
 		this.fenetre = p_fenetre;
+		this.taille = p_taille;
+		this.parametrerBarreOutils();
 	}
 
+	/* Ajoute les différents objets graphiques qui doivent s'afficher dans la barre d'outils */
     public void ajouterObjetsGraphiques() {
+		/* Instanciations et déclarations des différents boutons */
 		Btn useCase = new BtnUseCase("usecase", this.fenetre);
 		Btn classe = new BtnClasse("classe", this.fenetre);
 		Btn acteurActif = new BtnActeurActif("acteurActif", this.fenetre);
 		Btn acteurPassif = new BtnActeurPassif("acteurPassif", this.fenetre);
         Btn traitement = new BtnTraitement("traitement", this.fenetre);
         Btn association = new BtnAssociation("association", this.fenetre);
-        Btn generalisation = new BtnGeneralisation("generalisation", this.fenetre);        
+        Btn generalisation = new BtnGeneralisation("generalisation", this.fenetre);
         Btn agregation = new BtnAgregation("agregation", this.fenetre);
-        Btn composition = new BtnComposition("composition", this.fenetre);        
+        Btn composition = new BtnComposition("composition", this.fenetre);
+
+		/* Ajouts des boutons dans la barre d'outils */
 		this.add(useCase);
 		this.add(classe);
-		this.add(acteurActif);  
+		this.add(acteurActif);
         this.add(acteurPassif);
         this.add(traitement);
         this.add(association);
         this.add(generalisation);
-        this.add(agregation);        
-        this.add(composition);        
+        this.add(agregation);
+        this.add(composition);
     }
 
 }
