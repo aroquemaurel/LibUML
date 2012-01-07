186f6fb73aacb7d2c4e44a09b2706815b5c2bcab
Marie-Ly Tang <marie-ly.tang@hotmail.fr>
2012-01-07 11:05:57 +0100

    Ajout des commentaires javadoc

--- BibliothequeUML/diagramme/DiagrammeClasse.java
+++ BibliothequeUML/diagramme/DiagrammeClasse.java
@@ -5,11 +5,20 @@ import eltGraphique.classe.Classe;
 import eltGraphique.ligne.Lien;
 
 /**
- *
+ * Classe propre au diagramme de classe
+ * 
  * @author satenske
  */
  
 public class DiagrammeClasse extends Diagramme {
+    /**
+     * vérifie si le lien est autorisé
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @param p_typeDeFleche
+     * @return true s'il est autorisé, false sinon
+     */
     @Override
     public boolean lienAutorise (EltGraphique p_origine , EltGraphique p_extremite, String p_typeDeFleche){
         boolean valeurRetour = false;
@@ -39,6 +48,14 @@ public class DiagrammeClasse extends Diagramme {
         return valeurRetour;
     }
     
+    /**
+     * méthode qui autorise le lien d'association
+     * en fonction de l'origine et de l'extrémité
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @return true s'il est autorisé, false sinon
+     */
     private boolean autorisationAssociation(EltGraphique p_origine , EltGraphique p_extremite) {
         boolean valeurRetour = false;
         if(p_origine instanceof Classe && p_extremite instanceof Classe){
@@ -52,7 +69,15 @@ public class DiagrammeClasse extends Diagramme {
         }     
         return(valeurRetour);
     }
-
+    
+    /**
+     * méthode qui autorise le lien d'agrégation
+     * en fonction de l'origine et de l'extrémité
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @return true s'il est autorisé, false sinon
+     */
     private boolean autorisationAgregation(EltGraphique p_origine , EltGraphique p_extremite) {
         boolean valeurRetour = false;
         if(p_origine instanceof Classe && p_extremite instanceof Classe){
@@ -60,7 +85,15 @@ public class DiagrammeClasse extends Diagramme {
         }
         return(valeurRetour);
     }
-
+    
+    /**
+     * méthode qui autorise le lien de composition
+     * en fonction de l'origine et de l'extrémité
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @return true s'il est autorisé, false sinon
+     */
     private boolean autorisationComposition(EltGraphique p_origine , EltGraphique p_extremite) {
            boolean valeurRetour = false;
         if(p_origine instanceof Classe && p_extremite instanceof Classe){
@@ -68,7 +101,15 @@ public class DiagrammeClasse extends Diagramme {
         }
         return(valeurRetour);
     }
-
+    
+    /**
+     * méthode qui autorise le lien de généralisation
+     * en fonction de l'origine et de l'extrémité
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @return true s'il est autorisé, false sinon
+     */
     private boolean autorisationGeneralisation(EltGraphique p_origine , EltGraphique p_extremite) {
            boolean valeurRetour = false;
         if(p_origine instanceof Classe && p_extremite instanceof Classe){
@@ -76,7 +117,15 @@ public class DiagrammeClasse extends Diagramme {
         }
         return(valeurRetour);
     }
-
+    
+    /**
+     * méthode qui autorise le lien de dépendance
+     * en fonction de l'origine et de l'extrémité
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @return true s'il est autorisé, false sinon
+     */
     private boolean autorisationDependance(EltGraphique p_origine , EltGraphique p_extremite) {
            boolean valeurRetour = false;
         if(p_origine instanceof Classe && p_extremite instanceof Classe){
@@ -90,6 +139,13 @@ public class DiagrammeClasse extends Diagramme {
         }     
         return(valeurRetour);
     }
+    
+    /**
+     * vérifie si l'élément est autorisé
+     * 
+     * @param p_element
+     * @return true s'il est autorisé, false sinon
+     */
     @Override
     public boolean eltAutorise (EltGraphique p_element){
         boolean valeurRetour = false;
