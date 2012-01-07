cba52126f33c4848288df80fc0e92d71d11f0927
Marie-Ly Tang <marie-ly.tang@hotmail.fr>
2012-01-07 11:04:49 +0100

    Ajout des commentaires javadoc

--- BibliothequeUML/diagramme/Diagramme.java
+++ BibliothequeUML/diagramme/Diagramme.java
@@ -4,7 +4,11 @@ import eltGraphique.EltGraphique;
 import java.util.List;
 
 /**
- *
+ * Classe ancêtre de trois diagrammes: 
+ * Classe, CasUtilisation et Séquence
+ * Ces trois types de diagrammes sont différenciés 
+ * par les éléments qui les constituent
+ * 
  * @author satenske
  */
 public class Diagramme {
@@ -12,9 +16,17 @@ public class Diagramme {
      * liste des éléments graphiques
      */
     private List<EltGraphique> eltGraphique;
+    /**
+     * vérifie si le lien est autorisé
+     * @return true s'il est autorisé
+     */
     public boolean lienAutorise (EltGraphique p_origine , EltGraphique p_extremite, String p_typeDeFleche){
         return true;
     }
+    /**
+     * vérifie si l'élément est autorisé
+     * @return true s'il est autorisé
+     */
     public boolean eltAutorise (EltGraphique p_element){
         return true;
     }
