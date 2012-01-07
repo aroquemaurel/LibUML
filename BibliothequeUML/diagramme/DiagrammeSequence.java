9e4951521e9638128302dc109d1a210ae0745394
Marie-Ly Tang <marie-ly.tang@hotmail.fr>
2012-01-07 11:06:30 +0100

    Ajout des commentaires javadoc

--- BibliothequeUML/diagramme/DiagrammeSequence.java
+++ BibliothequeUML/diagramme/DiagrammeSequence.java
@@ -7,10 +7,20 @@ import eltGraphique.Traitement;
 import eltGraphique.ligne.Lien;
 
 /**
- *
+ * Classe propre au diagramme de séquence
+ * 
  * @author satenske
  */
 public class DiagrammeSequence extends Diagramme {
+    
+     /**
+     * vérifie si le lien est autorisé
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @param p_typeDeFleche
+     * @return true s'il est autorisé, false sinon
+     */
     @Override
     public boolean lienAutorise (EltGraphique p_origine , EltGraphique p_extremite, String p_typeDeFleche){
         boolean valeurRetour = false;
@@ -25,6 +35,14 @@ public class DiagrammeSequence extends Diagramme {
         return valeurRetour;
     }
     
+     /**
+     * méthode qui autorise le lien d'association
+     * en fonction de l'origine et de l'extrémité
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @return true s'il est autorisé, false sinon
+     */
     private boolean autorisationAssociation(EltGraphique p_origine , EltGraphique p_extremite) {
         boolean valeurRetour = false;
         if(p_origine instanceof Traitement && p_extremite instanceof Traitement){
@@ -32,7 +50,15 @@ public class DiagrammeSequence extends Diagramme {
         }
         return(valeurRetour);
     }
-
+    
+     /**
+     * méthode qui autorise le lien de dépendance
+     * en fonction de l'origine et de l'extrémité
+     * 
+     * @param p_origine
+     * @param p_extremite
+     * @return true s'il est autorisé, false sinon
+     */
     private boolean autorisationDependance(EltGraphique p_origine , EltGraphique p_extremite) {
            boolean valeurRetour = false;
         if(p_origine instanceof Traitement && p_extremite instanceof Traitement){
@@ -40,6 +66,13 @@ public class DiagrammeSequence extends Diagramme {
         }
         return(valeurRetour);
     }
+    
+    /**
+     * vérifie si l'élément est autorisé
+     * 
+     * @param p_element
+     * @return true s'il est autorisé, false sinon
+     */
     @Override
     public boolean eltAutorise (EltGraphique p_element){
         boolean valeurRetour = false;
