73b9b6dd03a61deb00d9dab397c400e63581fd49
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-11 09:55:14 +0100

    Ajout de la classe de test LienTest pour la classe Lien

---
+++ Tests/eltGraphique/LienTest.java
@@ -0,0 +1,31 @@
+package eltGraphique;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mathieu
+ */
+public class LienTest {
+	
+	public LienTest() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+	
+	@Test
+	public void test() {
+		fail();
+	}
+}
