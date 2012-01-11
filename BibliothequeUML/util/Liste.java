7fbd9af3a67a84ac16caa5fc7228eb7d1c0051dd
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-11 11:49:14 +0100

    Refactoring de la classe Liste dans le package util

---
+++ BibliothequeUML/util/Liste.java
@@ -0,0 +1,58 @@
+package util;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class Liste<T> extends ArrayList<T> {
+    
+    /**
+     * Construit une liste vide
+     */
+    public Liste(){
+        super();
+    }
+    
+    /**
+     * Ajouter un élément à la fin de la liste
+     * @param p_element
+     */
+    public void ajouterElement(T p_element){
+        super.add(p_element);
+    }
+    
+    /**
+     * Supprimer l'élément à l'index spécifié
+     * @param p_index le rang à supprimer
+     * @return l'élément supprimé
+     */
+    public T supprimerElement(int p_index){
+        return (super.remove(p_index));
+    }
+
+    /**
+     * Retour l'élément à l'index spécifié
+     * @param p_index Le numéro de l'élément à retourner
+     * @return L'élément à la place p_index
+     */
+    public T getElement(int p_index){
+        return (super.get(p_index));
+    }
+        
+    /**
+     * Retourne vrai si la liste est vide
+     * @return si la liste est vide ou non
+     */
+    public boolean estVide(){
+        return (super.isEmpty());
+    }
+    
+    /**
+     * Retourne le nombre d'éléments de la liste
+     * @return La taille de la liste
+     */
+    public int taille(){
+        return (super.size());
+    }
+}
