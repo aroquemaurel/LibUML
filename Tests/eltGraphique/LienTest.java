0b5eb5ea46b779b84546699f91275de8929f48a8
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-11 10:25:40 +0100

    Création méthodes de test pour ClasseTest.java

--- Tests/eltGraphique/LienTest.java
+++ Tests/eltGraphique/LienTest.java
@@ -1,9 +1,7 @@
 package eltGraphique;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
