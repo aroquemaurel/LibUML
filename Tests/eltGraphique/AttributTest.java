ac2cbcb631614d3cf5229aeb07870d6540b5ee9f
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-12 12:13:23 +0100

    Ajout tests des attributs pour Attribut

--- Tests/eltGraphique/AttributTest.java
+++ Tests/eltGraphique/AttributTest.java
@@ -1,22 +1,35 @@
 package eltGraphique;
 
+import eltGraphique.classe.Attribut;
+import eltGraphique.classe.Visibilite;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AttributTest {
+	private Attribut attribut;
 	
 	@Before
 	public void setUp() {
+		this.attribut = new Attribut(Visibilite.PRIVATE, false, false, "String", "attributPourLesTests");
 	}
 	
 	@After
 	public void tearDown() {
+		this.attribut = null;
 	}
 	
 	@Test
-	public void testFail() {
-		fail();
+	public void testVisibilite() {
+		this.attribut.setVisibilite(Visibilite.PACKAGE);
+		assertEquals(Visibilite.PACKAGE, this.attribut.getVisibilite());
 	}
+	
+	@Test
+	public void testDeClasse() {
+		this.attribut.setDeClasse(true);
+		assertTrue(this.attribut.estDeClasse());
+	}
+	
 }
