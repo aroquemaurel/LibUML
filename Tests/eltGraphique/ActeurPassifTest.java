a38d1db1d931efa313297f00a83fc542559337c3
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-12 15:48:50 +0100

    Complément des tests des acteurs actifs et passifs

--- Tests/eltGraphique/ActeurPassifTest.java
+++ Tests/eltGraphique/ActeurPassifTest.java
@@ -1,5 +1,8 @@
 package eltGraphique;
 
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.view.mxStylesheet;
+import util.Constantes;
 import diagramme.Diagramme;
 import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxICell;
@@ -13,65 +16,85 @@ import static org.junit.Assert.*;
  *
  */
 public class ActeurPassifTest {
-	private ElementModelisation monActeur;
+	private ElementModelisation acteur;
 
     @Before
     public void setUp() {
-		this.monActeur = new ActeurPassif(new mxGraph(), 
-                                                  new Diagramme(), 
-                                                  "un super test");
-        this.monActeur.creer();
+		this.acteur = new ActeurPassif(new mxGraph(), 
+									   new Diagramme(), 
+									   "un super test");
+        this.acteur.creer();
     }
 
     @After
     public void tearDown() {
-		this.monActeur = null;
+		this.acteur = null;
     }
 
     @Test
-    public void testGetCellule(){
-        mxICell maCellule1 = this.monActeur.getParent();
-        assertEquals(maCellule1, this.monActeur.getCellule());
-
-        mxICell maCellule2 = this.monActeur.getTarget();
-        assertEquals(maCellule2, this.monActeur.getCellule());
+    public void testCelluleParent(){
+        mxICell cellule = this.acteur.getParent();
+        assertEquals(cellule, this.acteur.getCellule());
+	}
+	
+	@Test
+		public void testCelluleTarget() {
+        mxICell cellule = this.acteur.getTarget();
+        assertEquals(cellule, this.acteur.getCellule());
 	}
 
 	@Test
-	public void testCreer1 (){
-        mxICell maCellule = this.monActeur.getCellule();
-        assertEquals(maCellule, this.monActeur.getCellule());
+	public void testCellule(){
+        mxICell cellule = this.acteur.getCellule();
+        assertEquals(cellule, this.acteur.getCellule());
 	}
 
 	@Test
-	public void testCreer2(){
-		assertTrue(this.monActeur instanceof ActeurPassif);
+	public void testInstance(){
+		assertTrue(this.acteur instanceof ActeurPassif);
 	}
 
 	@Test
-	public void testCreer3(){
-		assertEquals(null, this.monActeur.getCellule().getStyle());
+	public void testCreationStyle() {
+		mxStylesheet feuilleStyles = this.acteur.getGraph().getStylesheet();
+		assertTrue(feuilleStyles.getStyles().containsKey("ACTEUR_PASSIF"));
+		assertEquals(0, feuilleStyles.getStyles().get("ACTEUR_PASSIF").get(mxConstants.STYLE_AUTOSIZE));
+		assertEquals(0, feuilleStyles.getStyles().get("ACTEUR_PASSIF").get(mxConstants.STYLE_RESIZABLE));
+		assertEquals(mxConstants.SHAPE_RECTANGLE, feuilleStyles.getStyles().get("ACTEUR_PASSIF").get(mxConstants.STYLE_SHAPE));
+		assertEquals(Constantes.OPACITE, feuilleStyles.getStyles().get("ACTEUR_PASSIF").get(mxConstants.STYLE_OPACITY));
+		assertEquals(Constantes.COULEUR_TEXTE, feuilleStyles.getStyles().get("ACTEUR_PASSIF").get(mxConstants.STYLE_FONTCOLOR));
+		assertEquals(Constantes.COULEUR_BORDURE, feuilleStyles.getStyles().get("ACTEUR_PASSIF").get(mxConstants.STYLE_STROKECOLOR));
 	}
-
+	
+	@Test
+	public void testStyle(){
+		assertEquals("ACTEUR_PASSIF", this.acteur.getCellule().getStyle());
+	}
 
     @Test
     public void testSetTexte(){
-        this.monActeur.setTexte("testouille");
-        assertEquals("testouille", this.monActeur.getTexte());
+        this.acteur.setTexte("Nouveau Texte");
+        assertEquals("Nouveau Texte", this.acteur.getTexte());
     }
 
 	@Test
-	public void testSetCellule(){
+	public void testSetCelluleParent(){
 		mxCell maCellulle = new mxCell();
-		this.monActeur.setCellule(maCellulle);
-		assertEquals(maCellulle, this.monActeur.getParent());
-		assertEquals(maCellulle, this.monActeur.getTarget());
+		this.acteur.setCellule(maCellulle);
+		assertEquals(maCellulle, this.acteur.getParent());
+	}
+	
+	@Test
+	public void testSetCelluleTarget(){
+		mxCell maCellulle = new mxCell();
+		this.acteur.setCellule(maCellulle);
+		assertEquals(maCellulle, this.acteur.getTarget());
 	}
 
 	@Test
     public void testSupprimer(){
-        this.monActeur.supprimer();
-        assertNull(this.monActeur.getCellule());
+        this.acteur.supprimer();
+        assertNull(this.acteur.getCellule());
     }
 
 
