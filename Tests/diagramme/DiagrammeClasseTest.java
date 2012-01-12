d8c5c68f91e3f6ae555af3078b834b76dc841b9c
Marie-Ly Tang <marie-ly.tang@hotmail.fr>
2012-01-12 08:44:07 +0100

    Ajout des fichiers de test des diagrammes

---
+++ Tests/diagramme/DiagrammeClasseTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diagramme;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Marie-Ly
+ */
+public class DiagrammeClasseTest {
+    
+    private monDiagramme
+    
+    public DiagrammeClasseTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        this.monDiagramme
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
