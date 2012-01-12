886e623f05913b875532fb80f4bad22d88c10e9b
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-12 11:53:13 +0100

    Ajout de la classe de test pour Attribut

---
+++ Tests/eltGraphique/AttributTest.java
@@ -0,0 +1,22 @@
+package eltGraphique;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AttributTest {
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+	
+	@Test
+	public void testFail() {
+		fail();
+	}
+}
