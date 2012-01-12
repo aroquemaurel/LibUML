79c54271520bd9eb4db3eb16a9c14fbbc711b2b4
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-12 14:34:20 +0100

    Ajout de la classe de test de Variable

---
+++ Tests/eltGraphique/VariableTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eltGraphique;
+
+import eltGraphique.classe.Variable;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mathieu
+ */
+public class VariableTest {
+	private Variable variable;
+	
+	@Before
+	public void setUp() {
+		this.variable = new Variable(false, "Object", "variablePourLesTests");
+	}
+	
+	@After
+	public void tearDown() {
+		this.variable = null;
+	}
+	
+	@Test
+	public void testConstante() {
+		this.variable.setConstante(true);
+		assertTrue(this.variable.estConstante());
+	}
+	
+	@Test
+	public void testType() {
+		this.variable.setType("NouveauType");
+		assertEquals("NouveauType", this.variable.getType());
+	}
+	
+	@Test
+	public void testNom() {
+		this.variable.setNom("nouveauNom");
+		assertEquals("nouveauNom", this.variable.getNom());
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals("variablePourLesTests:Object", this.variable.toString());
+	}
+}
