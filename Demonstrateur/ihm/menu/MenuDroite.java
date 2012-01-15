106c27351f5b24bc2aca0fcfd40eea50f73a3d95
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2012-01-15 18:49:51 +0100

    Suppresion code inutile

--- Demonstrateur/ihm/menu/MenuDroite.java
+++ Demonstrateur/ihm/menu/MenuDroite.java
@@ -6,7 +6,6 @@ import java.awt.Dimension;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.TableColumn;
-import util.Liste;
 
 /**
 * Menu contenant le tableau avec les données
@@ -16,29 +15,9 @@ public class MenuDroite extends Menu {
         super(p_dimension, p_fenetre);
     }
 
-    /* Ajoute les objets graphiques du Menu (le tableau) */
+    /*  */
     @Override
-    public void ajouterObjetsGraphiques() {/*
-        Object[][] donnees = {
-                {"Private", "boolean", "jeSuisUnattribut","o","n"},
-                {"Protected", "int", "jeSuisUnattribut","o","n"},
-                {"Protected", "int", "jeSuisUnattribut","o","n"},
-                {"Protected", "int", "jeSuisUnattribut","o","n"},
-                                {"Protected", "int", "jeSuisUnattribut","o","n"},
-                                                {"Protected", "int", "jeSuisUnattribut","o","n"},
-                {"Private", "double", "jeSuisUnattribut","o","n"},
-                {"Private", "double", "jeSuisUnattribut","o","n"},
-                {"Private", "float", "jeSuisUnattribut","o","n"}
-            };
-        //Les titres des colonnes
-        String  title[] = {"Visibilité", "Type", "Nom", "Static", "Final"};
-        JTable tableau = new JTable(donnees, title);
-
-        tableau.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        TableColumn col = tableau.getColumnModel().getColumn(2);
-     //    col.setPreferredWidth(200);
-        super.add(new JScrollPane(tableau));*/
-    }
+    public void ajouterObjetsGraphiques() {}
 
     public void afficherTableauClasse(Classe classe){
         Object[][] donnees = new Object[classe.getAttributs().size()][5];
