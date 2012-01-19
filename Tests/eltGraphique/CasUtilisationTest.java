1bc1820e8aa9d43cc62146c9367b5dd821cae3c8
Mathieu Soum <mathieu.soum@gmail.com>
2012-01-19 16:16:25 +0100

    Ajout de la javadoc pour CasUtilisationTest

--- Tests/eltGraphique/CasUtilisationTest.java
+++ Tests/eltGraphique/CasUtilisationTest.java
@@ -15,11 +15,20 @@ import util.Constantes;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Cas de test JUnit regroupant les tests unitaires de chaque méthode
+ * de la classe CasUtilisation
+ * 
+ * @see CasUtilisation
  */
 public class CasUtilisationTest {
+	/**
+	 * Le champ sur lequel on effectuera les tests
+	 */
 	private ElementModelisation casUtilisation;
 
+	/**
+	 * Initialisation du champ avant chaque test
+	 */
     @Before
     public void setUp() {
 		this.casUtilisation= new CasUtilisation(new mxGraph(), 
@@ -28,65 +37,101 @@ public class CasUtilisationTest {
         this.casUtilisation.creer();
     }
 
+	/**
+	 * Suppression de ce champ après chaque test
+	 */
     @After
     public void tearDown() {
 		this.casUtilisation = null;
     }
 
+	/**
+	 * Test unitaire qui teste le cas d'utilisation n'a pas d'élément parent
+	 * 
+	 * Vérifie que :
+	 *  - la cellule parente est la même que la cellule actuelle
+	 */
     @Test
     public void testCelluleParent() {
         mxICell cellule = this.casUtilisation.getParent();
         assertEquals(cellule, this.casUtilisation.getCellule());
 	}
 	
+	/**
+	 * Test unitaire qui teste le cas d'utilisation n'a pas d'élément cible
+	 * 
+	 * Vérifie que :
+	 *  - la cellule cible est la même que la cellule actuelle
+	 */
     @Test
     public void testCelluleTarget() {
-
-    mxICell cellule = this.casUtilisation.getTarget();
-    assertEquals(cellule, this.casUtilisation.getCellule());
+		mxICell cellule = this.casUtilisation.getTarget();
+		assertEquals(cellule, this.casUtilisation.getCellule());
     }
 
+	/**
+	 * Test unitaire que teste l'instanciation du champ
+	 * 
+	 * Vérifie que :
+	 *  - le champ est une instance de la classe CasUtilisation
+	 */
     @Test
     public void testInstance(){
-            assertTrue(this.casUtilisation instanceof CasUtilisation);
+		assertTrue(this.casUtilisation instanceof CasUtilisation);
     }
 
+	/**
+	 * Test unitaire qui vérifie l'application du style sur le cas d'utilisation
+	 * en recupérant le style de la cellule liée à le cas d'utilisation
+	 * 
+	 * Vérifie que :
+	 *  - le style de la cellule est USECASE
+	 */
     @Test
     public void testStyle(){
-            assertEquals("USECASE", this.casUtilisation.getCellule().getStyle());
+		assertEquals("USECASE", this.casUtilisation.getCellule().getStyle());
     }
 
-    @Test
-    public void testCreerStyle() {
-            assertTrue(this.casUtilisation.getGraph().getStylesheet().getStyles().containsKey("USECASE"));
-    }
-
+	/**
+	 * Test unitaire qui teste la création du style du cas d'utilisation
+	 * en vérifiant chaque champ du nouveau style
+	 * 
+	 * Vérifie que :
+	 *  - le style est créé
+	 *  - la forme est celle du cas d'utilisation
+	 *  - l'inclusion d'un autre élément soit impossible
+	 *  - l'opacite est correcte
+	 *  - la couleur du texte est correcte
+	 *  - la couleur de la bordure est correcte
+	 */
     @Test
     public void testChampsStyle() {
-            mxStylesheet feuilleStyles = this.casUtilisation.getGraph().getStylesheet();
-            assertEquals(mxConstants.SHAPE_ELLIPSE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_SHAPE));
-            assertEquals(Constantes.OPACITE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_OPACITY));
-            assertEquals(Constantes.COULEUR_TEXTE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_FONTCOLOR));
-            assertEquals(Constantes.COULEUR_BORDURE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_STROKECOLOR));
+		mxStylesheet feuilleStyles = this.casUtilisation.getGraph().getStylesheet();
+		assertTrue(this.casUtilisation.getGraph().getStylesheet().getStyles().containsKey("USECASE"));
+		assertEquals(mxConstants.SHAPE_ELLIPSE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_SHAPE));
+		assertEquals(mxConstants.NONE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_FOLDABLE));
+		assertEquals(Constantes.OPACITE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_OPACITY));
+		assertEquals(Constantes.COULEUR_TEXTE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_FONTCOLOR));
+		assertEquals(Constantes.COULEUR_BORDURE, feuilleStyles.getStyles().get("USECASE").get(mxConstants.STYLE_STROKECOLOR));
     }
 
     @Test
     public void testSetTexte(){
-                this.casUtilisation.setTexte("testouille");
+		this.casUtilisation.setTexte("testouille");
         assertEquals("testouille", this.casUtilisation.getTexte());
     }
 
     @Test
     public void testSetCellule(){
-            mxCell maCellulle = new mxCell();
-            this.casUtilisation.setCellule(maCellulle);
-            assertEquals(maCellulle, this.casUtilisation.getParent());
-            assertEquals(maCellulle, this.casUtilisation.getTarget());
+		mxCell maCellulle = new mxCell();
+		this.casUtilisation.setCellule(maCellulle);
+		assertEquals(maCellulle, this.casUtilisation.getParent());
+		assertEquals(maCellulle, this.casUtilisation.getTarget());
     }
 
     @Test
     public void testSupprimer(){
-    this.casUtilisation.supprimer();
-    assertNull(this.casUtilisation.getCellule());
+		this.casUtilisation.supprimer();
+		assertNull(this.casUtilisation.getCellule());
     }
 }
