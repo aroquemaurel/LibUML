95614bb3a51fbdb2a78f30e073b341a8986f6eac
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2012-01-22 17:03:35 +0100

    Modification code test de charge

--- fournitures/plansEtCahiersDeTests/ChargeTest.java
+++ fournitures/plansEtCahiersDeTests/ChargeTest.java
@@ -16,45 +16,43 @@ public static void main(String[] args){
 	
 	/* créer des éléments */
 	for(int i = 0; i < 20 ; ++i) {
-		r = new Random();
-		position = new Position(valeurMinX + r.nextInt(valeurMaxX - valeurMinX), 
-				valeurMinY + r.nextInt(valeurMaxY - valeurMinY));
-		
+		/* on instancie une classe */
+		position = calculPositionAleatoire();
 		classes[i] = new Classe(fenetre.getPanneauGraph().getGraph(), 
 				new Diagramme(), "Test "+i, position);
-		r = new Random();
-		position = new Position(valeurMinX + r.nextInt(valeurMaxX - valeurMinX), 
-				valeurMinY + r.nextInt(valeurMaxY - valeurMinY));
 		
-		casUtilisations[i] = new CasUtilisation(fenetre.getPanneauGraph().getGraph(), 
-				new Diagramme(), "Test "+i, position);
-		r = new Random();
-		position = new Position(valeurMinX + r.nextInt(valeurMaxX - valeurMinX), 
-				valeurMinY + r.nextInt(valeurMaxY - valeurMinY));
-		
-		traitements[i] = new Traitement(fenetre.getPanneauGraph().getGraph(), 
-				new Diagramme(), "Test "+i, null, position, false);
-		r = new Random();
-		
-		position = new Position(valeurMinX + r.nextInt(valeurMaxX - valeurMinX), 
-				valeurMinY + r.nextInt(valeurMaxY - valeurMinY));
-		
-		acteursPassifs[i] = new ActeurPassif(fenetre.getPanneauGraph().getGraph(), 
-				new Diagramme(), "Test "+i, position);
-				
-		r = new Random();
-		position = new Position(valeurMinX + r.nextInt(valeurMaxX - valeurMinX), 
-				valeurMinY + r.nextInt(valeurMaxY - valeurMinY));
-		
-		acteursActifs[i] = new ActeurActif(fenetre.getPanneauGraph().getGraph(), 
+		/* on instancie un cas d'utilisation */
+		position = calculPositionAleatoire();
+		casUtilisations[i] = new CasUtilisation(
+				fenetre.getPanneauGraph().getGraph(), 
 				new Diagramme(), "Test "+i, position);
 
+		/* on instancie un traitement */
+		position = calculPositionAleatoire();
+		traitements[i] = new Traitement(
+				fenetre.getPanneauGraph().getGraph(), 
+				new Diagramme(), "Test "+i, null, position, false);
+		
+		/* on instancie un acteur passif*/
+		position = calculPositionAleatoire();
+		acteursPassifs[i] = new ActeurPassif(
+				fenetre.getPanneauGraph().getGraph(), 
+				new Diagramme(), "Test "+i, position);
+				
+		/* on instancie un acteur actif*/
+		position = calculPositionAleatoire();
+		acteursActifs[i] = new ActeurActif(
+				fenetre.getPanneauGraph().getGraph(), 
+				new Diagramme(), "Test "+i, position);
+
+		/* on créer tous les éléments instanciés précédemment */
 		classes[i].creer();
 		casUtilisations[i].creer();
 		traitements[i].creer();
 		acteursPassifs[i].creer();
 		acteursActifs[i].creer();
-		
+	
+		/* on relie les éléménts créer */
 		if(i != 0) {
 		   traitements[i].ajouterMessage(traitements[i-1], "test", TypeLien.ASSOCIATION);
 		}
@@ -62,6 +60,8 @@ public static void main(String[] args){
 				TypeLien.AGREGATION);
 		liens[i+2] = new Lien(acteursPassifs[i], casUtilisations[i], fenetre.getPanneauGraph().getGraph(), 
 				new Diagramme(), TypeLien.SPECIALISATION);
+
+		/* on affiche les liens */
 		liens[i+1].creer();
 		liens[i+2].creer();
 	}
